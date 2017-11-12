56a2b6d66fdadfcd0f4807d49f2a7866e1013ddf
Kmoscoso <katiemscs@gmail.com>
2017-11-12 10:55:24 +0100

    Formulario  inicial

--- client-web/src/app/app.component.ts
+++ client-web/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  public title = 'Music Rock!';
+  public user : User;
+  public identity ;
+  public token;
+
+  constructor(){this.user = new User('','','','','','ROLE_USER','')}
+  public onSubmit(){
+    console.log(this.user)
+  }
 }
---
+++ client-web/src/app/models/song.ts
@@ -0,0 +1,9 @@
+export class Song{
+    constructor(
+        public number : string,
+        public name: string,
+        public duration: number,
+        public file: string,
+        public album: string
+    ){}
+}
